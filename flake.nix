10c06ef8670a041a0917da1ad849f4b88e26e565
James Santucci <james.santucci@47deg.com>
2022-02-25 12:10:10 -0700

    include flake files
    flakes gets mad if you're in a git repo but haven't tracked the
    flake.nix and flake.lock files 🤷🏻‍♂️

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "An sbt plugin to monitor energy usage during command runs";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 self.lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          sbt = pkgs.sbt.override { jre = pkgs.openjdk8; };
+        in
+        {
+          energymonitor-plugin = pkgs.stdenv.mkDerivation {
+            name = "sbt-energymonitor";
+            buildInputs = [ sbt ];
+            src = ./.;
+          };
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.energymonitor-plugin);
+      devShell = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [ sbt ];
+        });
+
+    };
+}
+
