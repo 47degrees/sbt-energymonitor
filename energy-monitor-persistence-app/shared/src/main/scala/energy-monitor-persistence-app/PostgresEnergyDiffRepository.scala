ef4af148777af7fa1fbb808a2b897067805d4810
James Santucci <james.santucci@47deg.com>
2022-05-04 09:38:50 +0100

    implement rest of repository functions

--- energy-monitor-persistence-app/shared/src/main/scala/energy-monitor-persistence-app/PostgresEnergyDiffRepository.scala
+++ energy-monitor-persistence-app/shared/src/main/scala/energy-monitor-persistence-app/PostgresEnergyDiffRepository.scala
@@ -1,16 +1,15 @@
 package energymonitor.app
 
-import energymonitor.persistence.EnergyDiff
-import energymonitor.persistence.EnergyDiffRepository
+import energymonitor.persistence.{EnergyDiff, EnergyDiffRepository}
 
 import cats.effect.Concurrent
 import cats.effect.std.Console
-import cats.syntax.functor._
+import cats.syntax.all._
 import fs2.io.net.Network
 import natchez.Trace
 import skunk.codec.all._
 import skunk.implicits._
-import skunk.{Decoder, Encoder, Session, ~}
+import skunk.{AppliedFragment, Decoder, Encoder, Session, Void, ~}
 import squants.energy.Joules
 import squants.time.Seconds
 
@@ -48,7 +47,7 @@ class PostgresEnergyDiffRepository[F[
           ) ~ diff.run ~ diff.organization ~ diff.repository ~ diff.branch ~ diff.tag
       )
 
-    val decEnergyDiff: Decoder[EnergyDiff] =
+    private val decEnergyDiff: Decoder[EnergyDiff] =
       energyDiffCols.map {
         case joules ~ seconds ~ localDateTime ~ run ~ organization ~ repository ~ branch ~ tag =>
           EnergyDiff(
@@ -63,13 +62,48 @@ class PostgresEnergyDiffRepository[F[
           )
       }
 
+    private def energyDiffsFragment(
+        organization: String,
+        repository: String,
+        branch: Option[String],
+        fromRun: Option[Int],
+        toRun: Option[Int]
+    ) = {
+      val base = sql"""
+            select joules, duration_seconds, recorded_at, run, organization, repository, branch, tag
+            from energy_measurements
+            where
+              
+          """
+      val orgFilter = sql"organization = $text"
+      val repositoryFilter = sql"repository = $text"
+      val branchFilter = sql"branch = $text"
+      val fromRunFilter = sql"run >= $int4"
+      val toRunFilter = sql"run <= $int4"
+      val conditions = List(
+        Some(orgFilter(organization)),
+        Some(repositoryFilter(repository)),
+        branch.map(branchFilter),
+        fromRun.map(fromRunFilter),
+        toRun.map(toRunFilter)
+      ).flatten
+
+      val filter = conditions.foldSmash(
+        void" WHERE ",
+        void" AND ",
+        AppliedFragment.empty
+      )
+
+      base(Void) |+| filter
+    }
+
     def storeEnergyConsumptionMeasurement(
         energyDiff: EnergyDiff
     ): F[EnergyDiff] = pool.use { session =>
       session.use { s =>
         s.prepare(
           sql"""
-              insert into measurements (id, joules, duration_seconds, recorded_at, run, organization, repository, branch, tag)
+              insert into energy_measurements (id, joules, duration_seconds, recorded_at, run, organization, repository, branch, tag)
               values (
                   uuid_generate_v4(), $encEnergyDiff
               )
@@ -86,7 +120,27 @@ class PostgresEnergyDiffRepository[F[
         branch: Option[String],
         fromRun: Option[Int],
         toRun: Option[Int]
-    ): F[List[EnergyDiff]] = ???
+    ): F[List[EnergyDiff]] =
+      pool.use { session =>
+        session.use { s =>
+          val measurementsFragment =
+            energyDiffsFragment(
+              organization,
+              repository,
+              branch,
+              fromRun,
+              toRun
+            )
+
+          val query = measurementsFragment.fragment.query(decEnergyDiff)
+
+          s.prepare { query }
+            .use {
+              _.stream(measurementsFragment.argument, 64).compile.to(List)
+            }
+        }
+
+      }
 
     def meanEnergyConsumption(
         organization: String,
@@ -94,7 +148,33 @@ class PostgresEnergyDiffRepository[F[
         branch: String,
         fromRun: Option[Int],
         toRun: Option[Int]
-    ): F[Option[Double]] = ???
+    ): F[Option[Double]] = pool.use { session =>
+      session.use { s =>
+        val measurementsFragment: AppliedFragment =
+          energyDiffsFragment(
+            organization,
+            repository,
+            Some(branch),
+            fromRun,
+            toRun
+          )
+
+        s.prepare(measurementsFragment.fragment.query(decEnergyDiff))
+          .use {
+            _.stream(measurementsFragment.argument, 256)
+              .fold((0, 0d))((acc: (Int, Double), diff: EnergyDiff) => {
+                val (count, sum) = acc
+                (count + 1, sum + diff.joules.toJoules)
+              })
+              .compile
+              .to(List)
+              .map(_.headOption.flatMap {
+                case (0, _) => None
+                case (c, s) => Some(s / c.toDouble)
+              })
+          }
+      }
+    }
 
   }
 
